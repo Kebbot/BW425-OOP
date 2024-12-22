283b5fb58a27b0e6811f69bca0aad8198c2f12ac
Nikolz <126600682+Kebbot@users.noreply.github.com>
2024-12-22 18:40:44 +0300

    Commit#9
    dawdasd

--- BW425-OOP/BW425-OOP.cpp
+++ BW425-OOP/BW425-OOP.cpp
@@ -5,11 +5,10 @@
 #include <cmath>
 #include <io.h>
 #include <vector>
-#include <fstream>
+//#include <fstream>
 #include "Student.h"
 using namespace std;
-
-
+#define SQR(X) (X) * (X)
 class Date
 {
 
@@ -48,8 +47,6 @@ void Print(Date date)
     cout << date.day << '.' << date.month << '.' << date.year << endl;
 }
 
-
-
 class Array
 {
     int size;
@@ -81,6 +78,8 @@ class Point
 {
     double x;
     double y;
+    int size;
+    int* arr;
 public:
     
     Point(double x, double y) : x{x},y{y} {}
@@ -100,11 +99,16 @@ public:
     {
         return Point(point1.x /  value, point1.y / value);
     }
+    static const double Length(const Point& point)
+    {
+        double sum = SQR(point.x) + SQR(point.y);
+        return sqrt(sum);
+    }
     void Display() const
     {
         cout << "x = " << x << ' ' << "y = " << y << endl;
     }
-    // перегрузка дружественной функцией
+    // перегрузка дружественной функцией - Арифметические операторы
     friend Point operator+(const Point& point1, const Point& point2)
     {
         return Sum(point1, point2);
@@ -153,7 +157,152 @@ public:
         --(*this);
         return point;
     }
+
+    //перегрузка операторов сравнения
+    friend bool operator==(const Point& point1, const Point& point2)
+    {
+        return point1.x == point2.x && point1.y == point2.y;
+    }
+    friend bool operator!=(const Point& point1, const Point& point2)
+    {
+        return !(point1.x == point2.x && point1.y == point2.y);
+    }
+    friend bool operator>(const Point& point1, const Point& point2)
+    {
+        return Length(point1) > Length(point2);
+    }
+    
+    //перегрузка левого побитового сдвига
+    friend ostream& operator<<(ostream& output, const Point& point)
+    {
+        output << "(" << point.x << "," << point.y << ")";
+        return output;
+    }
+    friend istream& operator>>(istream& input, Point& point)
+    {
+        input >> point.x;
+        input.ignore(1);
+        input >> point.y;
+        return input;
+    }
+    double operator[](int value)
+    {
+        if (value == 0)
+            return x;
+        else if (value == 1)
+            return y;
+        else
+            cout << " i can`t find the index..." << endl;
+    }
 };
+
+//ДЗ от 18-12
+class Name
+{
+    char* firstName = nullptr;
+    char* secondName = nullptr;
+    void setCharArray(char*& dest, const char* source, Name name);
+    void remove();
+public:
+    void write();
+   
+};
+
+class DynArray
+{
+    int* arr;
+    int size;
+public:
+    explicit DynArray(int sizeP) : arr{ new int[sizeP] {} }, size{ sizeP } 
+    {
+        cout << "DynArray сконструировал массив на "
+            << size << " элементов в объекте " << this << endl;
+    }
+    DynArray() :DynArray(5){}
+    DynArray(const DynArray& object) :
+        arr{ new int[object.size] {} }, size{ object.size }
+    {
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = object.arr[i];
+        }
+        cout << "Конструктор копирования с размером "
+            << size << " элементов в объекте " << this << endl;
+    }
+    DynArray(DynArray&& object) : arr{ object.arr},size{ object.size}
+    {
+        object.arr = nullptr;
+        object.size = 0;
+        cout << "Конструктор перемещения с размером "
+            << size << " элементов в объекте " << this << endl;
+    }
+    DynArray& operator=(const DynArray& object)
+    {
+        //Проверка на самоприсваивание
+        if (!(this == &object))
+        {
+            /*проверка на невозможность "Переиспользовать"
+            блок памяти, выделенный под имеющийся массив*/
+            if (size != object.size)
+            {
+                delete[] arr;
+                arr = new int[object.size];
+            }
+            size = object.size;
+
+            //указатель на начало массива - назначения
+            int* dest{ arr }; 
+
+            //указатель на начало массива - источника
+            int* src{ object.arr };
+
+            //константный указатель на следующий 
+            // за последним элементом в массиве-назначения
+            //"Конец массива источника"
+            int* const end{ arr + size };
+            while (dest < end)
+            {
+                *dest++ = *src++;
+            }
+        }
+        cout << "DynArray скопировал данные размером "
+            << size << " элементов в объекте " << this << endl;
+        return *this;
+    }
+   
+    int getElem(int idx) const { return arr[idx]; }
+    void setElem(int idx, int val) { arr[idx] = val; }
+    void Print() const
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << arr[i] << ' ';
+        }
+        cout << endl;
+    }
+    void Randomize()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = rand() % 15;
+        }
+    }
+    ~DynArray()
+    {
+        cout << "Очистка памяти объекта DynArray в указателе " <<
+            arr << endl;
+        delete[] arr;
+        cout << "Сработал деструктор DynArray с размером "
+            << size << " элементов в объекте " << this << endl;
+    }
+};
+
+DynArray arrayFactory(int arrSize)
+{
+    DynArray arr{ arrSize };
+    arr.Randomize();
+    return arr; //можно move(arr) - для вызова конструктора перемещения
+}
 
 int main()
 {
@@ -163,11 +312,30 @@ int main()
     SetConsoleCP(1251);
     srand(time(NULL));
 
-    Point p1{ 1.1,1.5 };
-    Point p2{ 2,2 };
-    (p1++).Display();
-    p1.Display();
-
+    DynArray ar1{ arrayFactory(10) };
+    cout << "ar1 элементы: ";
+    ar1.Print();
+    DynArray ar2{ arrayFactory(10) };
+    cout << "ar2 элементы: ";
+    ar2.Print();
+    /*DynArray* ar1 = new DynArray[5]
+    {
+        {arrayFactory(15)},
+        {arrayFactory(5)},
+        {arrayFactory(7)},
+        {arrayFactory(3)},
+        {arrayFactory(20)}
+    };
+    for (int i = 0; i < 5; i++)
+    {
+        ar1[i].Print();
+    }
+    ar1[3] = ar1[0];
+    for (int i = 0; i < 5; i++)
+    {
+        ar1[i].Print();
+    }
+    delete[]ar1;*/
+    
 }
 
-//перегрузка операторов сравнения = след. тема
