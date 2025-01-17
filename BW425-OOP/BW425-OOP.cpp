d18597fefc518bd943fd794c9922f1ac7721b6ec
Nikolz <126600682+Kebbot@users.noreply.github.com>
2025-01-17 21:31:55 +0300

    Commit#15
    Para 2 - 17.01.2025

--- BW425-OOP/BW425-OOP.cpp
+++ BW425-OOP/BW425-OOP.cpp
@@ -12,166 +12,16 @@
 #include "Student.h"
 using namespace std;
 
-//шалон класса
-template <class T>
-class Array1
-{
-    T* array;
-    size_t size;
-public:
-    T getItem(size_t index) const;
-    void setItem(size_t index, T value);
-};
-
-// Функция принимаюя N количество параметров
-int getSum1(int count, int first, ...)
-{
-    int sum = 0;
-    int* p = &first;
-    while (count--)
-    {
-        sum += *p;
-        cout << p << endl;
-        p++;
-        cout << p << endl;
-        p++;
-        cout << p << endl;
-    }
-    return sum;
-}
-
-//double getSum(double x)
-//{
-//    return x;
-//}
-//
-//template <class... Args>
-//double getSum(double x, Args... args)
-//{
-//    return x + getSum(args...);
-//}
-
-template <class T>
-class DynArray
-{
-private:
-    int length;
-    T* data;
-public:
-    DynArray() : length(0), data(nullptr){}
-    DynArray(int length) : length{ length }
-    {
-        data = new T[length];
-    }
-    DynArray(const initializer_list<T>& list) : DynArray(list.size())
-    {
-        int i = 0;
-        for (auto& element : list )
-        {
-            data[i] = element;
-            i++;
-        }
-    }
-    ~DynArray()
-    {
-        delete[] data;
-    }
-    T& operator[](int index)
-    {
-        return data[index];
-    }
-    int getLength()const
-    {
-        return length;
-    }
-};
-
-struct Point
-{
-    int x;
-    int y;
-    friend ostream& operator<<(ostream& output, const Point& p)
-    {
-        output << "(" << p.x << "," << p.y << ")";
-        return output;
-    }
-};
-
-class Stack
-{
-    // Нижняя и верхняя границы
-    enum { EMPTY =-1, FULL=20};
-    
-    // Массив для хранения данных
-    char st[FULL + 1];
-
-    // Указатель на вершину
-    int top;
-public:
-    //конструктор по умолчанию
-    Stack()
-    {
-        //изначально стек пустой
-        top = EMPTY;
-    }
-
-    //добавление элемента
-    void Push(char c)
-    {
-        //Если в стеке есть место, то увеличиваем указатель на вершину
-        // стека и вставляем новый элемент
-        if (!isFull())
-            st[++top] = c;
-    }
-    
-    // Извлечение элемента
-    char Pop()
-    {
-        //Если в стеке есть элементы, то возвращаем верхний
-        //и уменьшаем указатель на вершину стека
-        if (!isEmpty())
-            return st[top--];
-        else //если нет элементов
-            return 0;
-    }
-
-    //очистка стека
-    void Clear()
-    {
-        /*Эффективная "очистка" стека
-        (данные в массиве все еще есть/существуют, 
-        но фунции класса, ориентированные на работу с вершиной стека,
-        будут их игнорировать)*/
-        top = EMPTY;
-    }
-
-    //проверка на наличие элементов
-    bool isEmpty()
-    {
-        //Стек пуст?
-        return top == EMPTY;
-    }
-
-    //проверка на переполнение стека
-    bool isFull()
-    {
-        //Стек полон?
-        return top == FULL;
-    }
-
-    //Кол-во элементов в стеке
-    int getCount()
-    {
-        return top;
-    }
-
-};
-
+//Вроженные классы
 class A
 {
 public:
-    int init(int);
+    void print()
+    {
+        obj->poo(*this);
+    }
 private:
+    int i = 10;
     class B;
     class Ref {
         // pli имеет тип A::B*
@@ -181,15 +31,26 @@ private:
     public:
         // pRef имеет тип A::Ref*
         Ref* pRef;
-        int value;
+        int value = 5;
         B* next; //указатель на собственный класс
+
         static int static_mem;
 
         B(int value = 0) /*: value{ value } {}*/;//Конструктор
         void mf( A&);
-       
+        int sum(A& ptr)
+        {
+            int sum = ptr.i + value + ptr.obj->value;
+            return sum;
+        }
+        void poo(A& ptr)
+        {
+            cout << ptr.i + value << endl;
+            cout << ptr.obj->value << endl;
+        }
+
     };
-    B* obj;
+    B* obj = new B;
 };
 
 //class A::B {
@@ -201,16 +62,13 @@ private:
 //};
 void A::B::mf( A& i1)
 {
-    int memb = i1.init(5);
+    
 }
 A::B::B(int value)
 {
     
 }
-
 int A::B::static_mem = 1024;
-
-
 class B
 {
 public:
@@ -218,12 +76,87 @@ public:
     A* obj;
 };
 
+//Агрегирование
+class Point
+{
+    int X;
+    int Y;
+public:
+    Point() {
+        X = Y = 0;
+    }
+    void setPoint(int iX, int iY) {
+        X = iX;
+        Y = iY;
+    }
+    void Show() {
+        cout << "------------------------------" << endl << endl;
+        cout << X << "\t" << Y << endl << endl;
+        cout << "------------------------------" << endl << endl;
+    }
+};
+
+class Figura {
+    //Агрегация точки - 
+    // (внутри одного класса (отдельного) находятся объект(ы) другого класса
+    Point* obj; 
+
+    int count; 
+    int color; 
+public:
+    Figura() {
+        count = color = 0;
+        obj = nullptr;
+    }
+    // Создание фигуры
+    void createFigura(int cr, int ct) {
+        //если углов меньше 3 - это не фигура
+        if (ct < 3) exit(0);
+        // инициализация света и кол-ва углов
+        count = ct;
+        color = cr;
+        //выделение памяти под массив точек
+        obj = new Point[count];
+
+        if(!obj)exit(0);
+
+        //установка координат точек
+        int tempX, tempY;
+        for (int i = 0; i < count; i++)
+        {
+            cout << "Set X\n";
+            cin >> tempX;
+            cout << "Set Y\n";
+            cin >> tempY;
+            obj[i].setPoint(tempX, tempY);
+        }
+    }
+    //показ фигуры
+    void ShowFigura() {
+        cout << "------------------------------" << endl << endl;
+        cout <<"Color - " << color << endl << endl;
+        cout << "Points - " << count << endl << endl;
+        for (int i = 0; i < count; i++)
+        {
+            obj[i].Show();
+        }
+        cout << "------------------------------" << endl << endl;
+    }
+
+    //Если фигура была, то очистить память
+    ~Figura(){
+        if (obj != nullptr) delete[] obj;
+    }
+};
+
 void Start()
 {
     bool choice = true;
     do {
         
-
+        Figura f;
+        f.createFigura(255, 3);
+        f.ShowFigura();
 
         cout << "Начать заново?\n" << "0 - Выход\n" 
             << "Всё остальное продолжает программу" << endl;
