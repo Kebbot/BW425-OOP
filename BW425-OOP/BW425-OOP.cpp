7a00708063746a0c9b18407b5547371ffa8bc7ed
Nikolz <126600682+Kebbot@users.noreply.github.com>
2025-02-05 18:40:26 +0300

    Commit#21
    Para 03.02.2025

--- BW425-OOP/BW425-OOP.cpp
+++ BW425-OOP/BW425-OOP.cpp
@@ -7,85 +7,17 @@
 #include <fstream>
 #include <iterator>
 #include <list>
-//#include "Student.h"
-
+#include <map>
+//#include "Student.h
 using namespace std;
 
-//dynamic_cast
-class B
+class Other
 {
 public:
-    //виртуальная функция
-    virtual void Test() {
-        cout << "Test" << endl;
-    }
+    int num;
+    string name;
+    void print();
 };
-class D : public B
-{
-public:
-    //переопределение виртуальной функции
-    void Test()
-    {
-        cout << "Test D" << endl;
-    }
-};
-
-//const_cast - Снимает атрибут const (или ставит) (работает с const и volatile)
-void test_pow(const int* v)
-{
-    int* tmp;
-    tmp = const_cast<int*>(v);
-    *tmp = *v * *v;
-}
-
-class FileOpen
-{
-    FILE* f;
-public: 
-    FileOpen(const char* fileName, const char* mode)
-    {
-        if (!(f = fopen(fileName, mode)))
-        {
-            exit(0);
-        }
-    }
-    ~FileOpen()
-    {
-        fclose(f);
-    }
-};
-
-void f() {
-    FileOpen myFile("Test.txt", "r+");
-}
-
-template <class InIter, class Dist>
-void advance(InIter& itr, Dist d);
-
-template <class InIter>
-ptrdiff_t distance(InIter& start, InIter end);
-
-//template <class T, class Allocator = Allocator<T> >
-//class List;
-
-typedef list<int> ourList;
-void ShowList(ourList& l1, ourList& l2)
-{
-    //создаем итератор
-    ourList::iterator iter;
-    cout << "List1: ";
-    for (iter = l1.begin(); iter != l1.end(); iter++)
-    {
-        cout << *iter << " ";
-    }
-    cout << endl << "List2: ";
-    for (iter = l2.begin(); iter != l2.end(); iter++)
-    {
-        cout << *iter << " ";
-    }
-    cout << endl << endl;
-}
-
 
 int main()
 {
@@ -95,56 +27,44 @@ int main()
     SetConsoleCP(1251);
     srand(time(NULL));
 
+    map<string, string> our_map;
+    vector<int> our_vector;
+    cout << "Max size vector: " << our_vector.max_size() / sizeof(int) << endl;
+    cout << "Max size map: \t" << our_map.max_size() / sizeof(int) << endl;
+    
+    string val;
+    string key;
 
-    list<int> list1, list2;
-    for (size_t i = 0; i < 6; i++)
+    cout << "Введи значение: ";
+    cin >> val;
+    cout << "Введи ключ: ";
+    cin >> key;
+
+    // создаем "Пару" на основании двух значений
+    pair<string, string> element(key, val);
+
+    our_map.insert(element); //втсавляем пару в отображение
+    cout << "Size map: \t" << our_map.size() << endl;
+
+    cout << "Введи значение: ";
+    cin >> val;
+    cout << "Введи ключ: ";
+    cin >> key;
+
+    pair<map<string, string>::iterator, bool>
+        err = our_map.insert(make_pair(key, val));
+
+    if (err.second == false)
     {
-        list1.push_back(i);
-        list2.push_back(i+i);
+        cout << "Errore" << endl;
     }
-    ShowList(list1, list2);
+    cout << "Current element: \t" << our_map.size() << endl;
 
-    list2.splice(list2.end(), list2, list2.begin());
-    ShowList(list1, list2);
-
-    list1.reverse();
-    ShowList(list1, list2);
-  
-    list1.sort();
-    list2.sort();
-    ShowList(list1, list2);
-
-    list1.merge(list2);
-    ShowList(list1, list2);
-
-    list1.unique();
-    ShowList(list1, list2);
-
-    
-    //const_cast - Снимает атрибут const (или ставит) (работает с const и volatile)
-
-    //dynamic_cast
-    //указатель на класс родитель
-    //B* ptr_b, obj_b;
-    ////указатель на класс потомок 
-    //D* ptr_d, obj_d;
-    ////приводим адрес объекта (D*) к указателю типа D*
-    //ptr_d = dynamic_cast<D*> (&obj_d);
-    ////если всё прошло успешно то вернется !0
- 
-    //reinterpret_cast
-    //int x; //Целое число
-    //const char* strT = { "This is string!!!" };
-    //char* str = new char[30];
-    //strcpy_s(str, 30, strT);
-    //cout << str << endl;
-    //x = reinterpret_cast<int>(str);
-    //cout << x << endl;
-    //str = reinterpret_cast<char*>(&x);
-    //cout << str << endl;
-
-    //static_cast - аналог (double)num;
-    
+    map<string, string>:: iterator iter = our_map.begin();
+    for (; iter != our_map.end();iter++)
+    {
+        cout << "Key: " << iter->first << "\t Value: " << iter->second <<endl;
+    }
     
     cout << "Конец программы" << endl;
     
